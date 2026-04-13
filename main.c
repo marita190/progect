9381270072bbde6adaaa0b0001f9f89da17e5932
marita190 <m.kashapova@g.nsu.ru>
2026-04-13 14:16:22 +0700

    update gaus, randon numbers

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "filter.h"
 
-int main(){
-	printf("Hello, World!");
-	return 0;
+int main() {
+    srand(time(NULL));
+    
+    //генерация чисел и считаем среднее
+    double sum = 0.0;
+    for (int i = 0; i < 1000; i++) {
+        sum += gaussian_noise(0.0, 1.0);
+    }
+    double mean = sum / 1000.0;
+    printf("%f\n", mean);
+    
+    return 0;
 }
